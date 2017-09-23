a61f4578b6c114c3d5c729458bd90162d92ed848
cdstelly <cdstelly@gmail.com>
2017-09-23 13:06:49 -0400

    imageinfo

--- goTSK.go
+++ goTSK.go
@@ -7,7 +7,10 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"os/exec"
+	"bytes"
 )
+
 
 type NugArg struct {
 	TheData []byte
@@ -15,6 +18,7 @@ type NugArg struct {
 
 type NugTSK struct {
 	SavedData []byte
+	PathToImage string
 }
 
 func (nd *NugTSK) GetDataLen(dataArg *NugArg, reply *string) error {
@@ -24,13 +28,31 @@ func (nd *NugTSK) GetDataLen(dataArg *NugArg, reply *string) error {
 
 func (nd *NugTSK) LoadData(dataArg *NugArg, reply *string) error {
 	nd.SavedData = dataArg.TheData
+
+	//we're going to hack it for now
+	// load /space/m57/jo-2009-11-24.E01 into memory
+	// actually.. no. just keep a reference ot it
+	nd.PathToImage = "/space/m57/jo-2009-11-24.E01"
+
 	*reply = "done"
 	return nil
 }
 
-func (nd *NugTSK) ExecInfo(dataArg *NugArg, reply *string) error {
-	*reply = "test"
-	return nil
+func (nd *NugTSK) ExecImageInfo(dataArg *NugArg, reply *string) error {
+	//todo: log the hash of the tool used? at least get version information
+	pathToExiftool := "/usr/bin/fsstat"
+	cmd := exec.Command(pathToExiftool, "-i", "ewf", "-o", "63", nd.PathToImage)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	fmt.Println(out.String())
+	*reply = out.String()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
 }
 
 func main() {
