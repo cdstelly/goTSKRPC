efefa5f2940b35aeb3238df9702c9849d85a9d3c
Christopher Stelly <cstelly1@uno.edu>
2018-02-22 05:28:06 -0600

    var name update

--- goTSK.go
+++ goTSK.go
@@ -12,6 +12,7 @@ import (
 	b64 "encoding/base64"
 	"os"
 	"io/ioutil"
+	"strings"
 )
 
 
@@ -31,21 +32,26 @@ func (nd *NugTSK) GetDataLen(dataArg *NugArg, reply *string) error {
 }
 
 func (nd *NugTSK) LoadData(dataArg *NugArg, reply *string) error {
+	if len(strings.Split(string(dataArg.TheData),":")) == 1 {
+		fmt.Println("[-] Data location did not contain type. Assuming file.")
+	}
+	fmt.Print("[-] Loading data from:  " + string(dataArg.TheData))
 	nd.SavedData = dataArg.TheData
 
 	//we're going to hack it for now
 	// load /space/m57/jo-2009-11-24.E01 into memory
 	// actually.. no. just keep a reference ot it
-	nd.PathToImage = "/space/m57/jo-2009-11-24.E01"
+	nd.PathToImage = string(dataArg.TheData)
 
 	*reply = "done"
+	fmt.Println(" .. done.")
 	return nil
 }
 
 func (nd *NugTSK) ExecImageInfo(dataArg *NugArg, reply *string) error {
 	//todo: log the hash of the tool used? at least get version information
-	pathToExiftool := "/usr/bin/fsstat"
-	cmd := exec.Command(pathToExiftool, "-i", "ewf", "-o", "63", nd.PathToImage)
+	pathToTool := "/usr/bin/fsstat"
+	cmd := exec.Command(pathToTool, "-i", "ewf", "-o", "63", nd.PathToImage)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
@@ -60,9 +66,10 @@ func (nd *NugTSK) ExecImageInfo(dataArg *NugArg, reply *string) error {
 }
 
 func (nd *NugTSK) GetBodyFile(dataArg *NugArg, reply *string) error {
-	pathToExiftool := "/usr/bin/fls"
+	pathToTool := "/usr/bin/fls"
 
-	cmd := exec.Command(pathToExiftool, "-rm", "/", "-o", "63", nd.PathToImage)
+	fmt.Print("[-] Getting body file")
+	cmd := exec.Command(pathToTool, "-rm", "/", "-o", "63", nd.PathToImage)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
@@ -73,15 +80,16 @@ func (nd *NugTSK) GetBodyFile(dataArg *NugArg, reply *string) error {
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(" .. done.")
 	return err
 }
 
 
 func (nd *NugTSK) GetFileData(dataArg *NugArg, reply *string) error {
-	pathToExiftool := "/usr/bin/icat"
+	pathToTool := "/usr/bin/icat"
 
-	cmd := exec.Command(pathToExiftool, "-o", "63", nd.PathToImage,  dataArg.Inode)
-outfile, err := os.Create("/space/out")
+	cmd := exec.Command(pathToTool, "-o", "63", nd.PathToImage,  dataArg.Inode)
+	outfile, err := os.Create("tmpICATout")
 	if err != nil {
 		panic(err)
 	}
@@ -93,7 +101,7 @@ outfile, err := os.Create("/space/out")
 	}
 	cmd.Wait()
 
-	filedata, err := ioutil.ReadFile("/space/out")
+	filedata, err := ioutil.ReadFile("tmpICATout")
 	if err != nil {
 		panic(err)
 	}
